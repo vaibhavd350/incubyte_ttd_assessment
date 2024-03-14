8c6616b2db0f31ba441168bd5f54b2fb9a2a52c5
Vaibhav Dwivedi <dvaibhav350@gmail.com>
2024-03-14 16:22:08 +0530

    Fix the numbers larger than 1000

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,5 @@
+# string_calculator.rb
+
 class StringCalculator
   def self.calculate(numbers)
     return 0 if numbers.empty?
@@ -5,7 +7,7 @@ class StringCalculator
     delimiters, numbers_string = extract_delimiters(numbers)
     numbers_array = parse_numbers(numbers_string, delimiters)
     validate_numbers(numbers_array)
-    numbers_array.sum
+    calculate_sum(numbers_array)
   end
 
   private
@@ -13,7 +15,7 @@ class StringCalculator
   def self.extract_delimiters(numbers)
     if numbers.start_with?("//")
       delimiter_section, numbers_section = numbers.split("\n", 2)
-      delimiters = delimiter_section[2..-1].split(/[\[\]]+/).reject(&:empty?)
+      delimiters = delimiter_section.gsub("//", "").split(/[\[\]]+/).reject(&:empty?)
       numbers_section ||= ""
     else
       delimiters = [",", "\n"]
@@ -28,7 +30,11 @@ class StringCalculator
   end
 
   def self.validate_numbers(numbers_array)
-    negatives = numbers_array.select(&:negative?)
+    negatives = numbers_array.select { |num| num.negative? }
     raise ArgumentError, "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+  end
+
+  def self.calculate_sum(numbers_array)
+    numbers_array.reject { |num| num > 1000 }.sum
   end
 end
