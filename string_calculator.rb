43e6fffa060cec1c307f972789116282508cea36
Vaibhav Dwivedi <dvaibhav350@gmail.com>
2024-03-14 16:18:02 +0530

    Add the string calculation functionality

---
+++ string_calculator.rb
@@ -0,0 +1,34 @@
+class StringCalculator
+  def self.calculate(numbers)
+    return 0 if numbers.empty?
+
+    delimiters, numbers_string = extract_delimiters(numbers)
+    numbers_array = parse_numbers(numbers_string, delimiters)
+    validate_numbers(numbers_array)
+    numbers_array.sum
+  end
+
+  private
+
+  def self.extract_delimiters(numbers)
+    if numbers.start_with?("//")
+      delimiter_section, numbers_section = numbers.split("\n", 2)
+      delimiters = delimiter_section[2..-1].split(/[\[\]]+/).reject(&:empty?)
+      numbers_section ||= ""
+    else
+      delimiters = [",", "\n"]
+      numbers_section = numbers
+    end
+    [delimiters, numbers_section]
+  end
+
+  def self.parse_numbers(numbers_string, delimiters)
+    delimiters_regex = Regexp.union(delimiters)
+    numbers_string.split(delimiters_regex).map(&:to_i)
+  end
+
+  def self.validate_numbers(numbers_array)
+    negatives = numbers_array.select(&:negative?)
+    raise ArgumentError, "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+  end
+end
